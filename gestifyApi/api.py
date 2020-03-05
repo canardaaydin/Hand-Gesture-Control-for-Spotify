919a049be0575d5a7a6212d796fd48bff6e98c53
canardaaydin <33313579+canardaaydin@users.noreply.github.com>
2020-03-05 23:22:20 +0300

    api index.html

--- gestifyApi/api.py
+++ gestifyApi/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 from flask_restful import Resource, Api, request
 import pprint
 
@@ -12,9 +12,10 @@ def extract_auth_code(str):
 class get_auth_code(Resource):
 	def get(self):
 		print(request.args.get('code'))
-		return render_template('index.html')
+		headers = {'Content-Type': 'text/html'}
+		return make_response(render_template('index.html'), 200,headers)
 
 api.add_resource(get_auth_code, '/')
 
 if __name__ == '__main__':
-    app.run(port=5000, host='0.0.0.0')
+    app.run(port=5000)
